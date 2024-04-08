da6a6915fba4de5903a60e76e54c92fa21aa1457
AldoZevana <aldo.kurti.14@gmail.com>
2024-04-08 18:37:34 +0200

    someCommit

---
+++ script.js
@@ -0,0 +1,17 @@
+var clicks_1 = 9;
+function ChangeOthers_1() {
+    clicks_1 += 1;
+    document.getElementById("clicks_1").innerHTML = clicks_1;
+};
+
+var clicks_2 = 12;
+function ChangeOthers_2() {
+    clicks_2 += 1;
+    document.getElementById("clicks_2").innerHTML = clicks_2;
+};
+
+var clicks_3 = 9;
+function ChangeOthers_3() {
+    clicks_3 += 1;
+    document.getElementById("clicks_3").innerHTML = clicks_3;
+};
